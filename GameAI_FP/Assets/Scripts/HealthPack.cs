bdf5f6d1f891b99cb15c0f8e312ba338a602a2fa
ben_jaccaci <jaccaci.b@northeastern.edu>
2026-04-06 22:02:50 -0400

    Healthpack

---
+++ GameAI_FP/Assets/Scripts/HealthPack.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour
+{
+    public int rotateSpeed = 50;
+    public int healthAmount = 20;
+    public AudioClip pickupSFX;
+    private AudioSource sfxAudioSource;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sfxAudioSource = gameObject.AddComponent<AudioSource>();
+        sfxAudioSource.playOnAwake = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.AddHealth(healthAmount);
+                sfxAudioSource.clip = pickupSFX;
+                sfxAudioSource.volume = 1.0f;
+                sfxAudioSource.Play();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
